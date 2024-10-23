f742e620153d4bc11027a1ea019f23a0e3c1072f
Erick Levandoski <Erick.ian.levandoski1@gmail.com>
2024-10-22 23:08:46 -0300

    Create recognizer

--- recognition.py
+++ recognition.py
@@ -1,2 +1,14 @@
 # Ouvir e detectar palavra-chave → Usar SpeechRecognition ou Vosk.
 
+import speech_recognition as sr
+
+rec = sr.Recognizer() # inicialize recognizer
+
+with sr.Microphone() as mic:
+    rec.adjust_for_ambient_noise(mic) # remove ambient noise
+    audio = rec.listen(mic) # record audio 
+    try:
+        text = rec.recognize_google(audio, language="pt-BR") # rocognize words in audio
+        print(text)
+    except:
+        print('Desculpe, não consegui escutar')
