1f8b2955dfeb9b7a018273ba4f0f5dff343e785a
Erick Levandoski <Erick.ian.levandoski1@gmail.com>
2024-10-23 22:06:08 -0300

    create intepreter

--- intepreter.py
+++ intepreter.py
@@ -1,2 +1,15 @@
-# Interpretar comandos → Usar spaCy ou transformers.
+# Interpretar Perguntas ->
+import google.generativeai as genai
+import os
+
+class Ia_Intepreter():
+    def __init__(self):      
+        genai.configure(api_key="AIzaSyC9bsa8KqT2Y7rv9L93VmT48TghLWlUQTY")
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+    def response(self, question):
+        response = self.model.generate_content(
+            question,
+        )
+        return response.text
 
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from intepreter import Ia_Intepreter
+from recognition import recognizer
 # Executar comandos do sistema → Usar os, pyautogui, e subprocess.
 # Falar respostas → Usar pyttsx3 ou gTTS.
 # Memória limitada → Usar SQLite ou TinyDB.
+
+
 
